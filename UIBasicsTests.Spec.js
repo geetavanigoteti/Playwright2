c1a0eb21fa5d649ff12a6252d772f0fd26aee859
Geeta <geeta@Geeta-ka-MacBook-Air.local>
2026-02-19 19:33:24 +0530

    jsscript1

---
+++ UIBasicsTests.Spec.js
@@ -0,0 +1,15 @@
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn)
+
+const BMIhigher = BMIMark > BMIJohn;
+if (BMIhigher) {
+    console.log(`Mark's BMI ${BMIMark} is higher than John's BMI ${BMIJohn}!`);
+} else {
+    console.log(`John's BMI ${BMIJohn} is higher than Mark's BMI ${BMIMark}!`);
+}
